e5657922bdd93c5a5b3d53ede0706328131ba9c7
vanya <vanchello089@gmail.com>
2023-09-28 22:27:15 +0600

    start project

--- spoti-clone/app/(site)/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
-            <Header>
-                Header
-            </Header>
-        </div>
-    )
-}
