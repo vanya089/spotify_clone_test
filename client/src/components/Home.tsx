b79a231a4db0a019c443971df614bcc5fe18d827
vanya <vanchello089@gmail.com>
2023-09-28 23:34:24 +0600

    start project

---
+++ client/src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div className="flex bg-red-800">
+fsadfsdfsdfsdf
+        </div>
+    );
+};
+
+export default Home;
